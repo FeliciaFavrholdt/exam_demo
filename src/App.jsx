f61a776e16aa71fd7489f845ecfe0b1afa06dc11
Felicia Favrholdt <feliciafavrholdt93@gmail.com>
2025-01-12 16:18:41 +0100

    changes in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,6 @@
-import React from "react";
-import ThemeToggle from "./ThemeToggle";
-
-function App() {
-  return (
-    <ThemeToggle>
-      <div>
-        <h1>Hello, World!</h1>
-        <p>This is a light/dark themed website.</p>
-      </div>
-    </ThemeToggle>
-  );
-}
-
-export default App;
+ const App = () => {
+   return (
+     <div>App</div>
+   )
+ }
+ export default App
