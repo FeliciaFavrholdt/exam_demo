06d7767747be6d84c6e9e1b943a269fbc3883c85
Felicia Favrholdt <feliciafavrholdt93@gmail.com>
2025-01-12 16:17:02 +0100

    adding light/dark theme toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
- const App = () => {
-   return (
-     <div>App</div>
-   )
- }
- export default App
+import React from "react";
+import ThemeToggle from "./ThemeToggle";
+
+function App() {
+  return (
+    <ThemeToggle>
+      <div>
+        <h1>Hello, World!</h1>
+        <p>This is a light/dark themed website.</p>
+      </div>
+    </ThemeToggle>
+  );
+}
+
+export default App;
